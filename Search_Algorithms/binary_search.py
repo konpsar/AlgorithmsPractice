5d1bfd2650a2abf19796f63a0e292e9f4005497c
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2021-06-16 22:50:15 +0300

    More detailed testing for binary_search

--- Search_Algorithms/binary_search.py
+++ Search_Algorithms/binary_search.py
@@ -1,5 +1,3 @@
-L=[2, 5, 6, 8, 9, 23, 45, 66, 78, 125, 456]
-
 def binary_search(L, x):
     #We spot the mid element
     i=0
@@ -12,13 +10,18 @@ def binary_search(L, x):
         elif x>mid_elem:
             #go to next loop with list indexes: 
             i = mid+1
-            print("indexes", i,j)
         elif x<mid_elem:
             #go to next loop with list indexes: 
             j = mid-1    
-            print("indexes", i,j)
     #if we get out of while without finding target element, it's not present in L
     return None
     
-for i in range(len(L)):
-    print(i, L[i], binary_search(L, L[i]))
+L = [2, 5, 6, 8, 9, 23, 45, 66, 78, 125, 456]
+K = [3, 4, 10]
+print(L)
+for i in range(len(L)): 
+    print("Search for {0:3d} which is in L with index {1:3d} | binary_search(L,{0:3d}) ->  {2:}"\
+        .format(L[i], L.index(L[i]), binary_search(L, L[i])))
+for i in range(len(K)):
+    print("Search for {0:3d} which is not in L | binary_search(L,{0:3d}) ->  {1:}"\
+        .format(K[i], binary_search(L, K[i])))
