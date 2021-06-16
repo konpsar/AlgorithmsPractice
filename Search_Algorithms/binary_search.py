e67c4a00172d83bee5a27ca1ee851bb43220f184
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2021-06-16 23:16:22 +0300

    Added recursive implementation of binary search and corresponding tests

--- Search_Algorithms/binary_search.py
+++ Search_Algorithms/binary_search.py
@@ -10,9 +10,26 @@ def binary_search(L, x):
         elif x>mid_elem:
             #go to next loop with list indexes: 
             i = mid+1
-        elif x<mid_elem:
+        else:
             #go to next loop with list indexes: 
             j = mid-1    
     #if we get out of while without finding target element, it's not present in L
+    return None
+    
+def binary_search_recursive(L, x, i=0, j=-1):
+    assert i>=0, j>=0
+    if i <= j:
+        mid = (i+j)//2
+        mid_elem = L[mid]
+        if  x == mid_elem:
+            return mid
+        elif x> mid_elem:
+            # recursive call with left sublist - add mid to index to correct misaligned index
+            return binary_search_recursive(L, i+1, j, x)
+        else:
+            # recursive call with right sublist
+            return binary_search_recursive(L, i, j-1, x)
+    # if first if is not confirmed and target elem is not found
+    # then it's not present in L
     return None
     
