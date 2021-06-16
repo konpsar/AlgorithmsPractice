374bc92f4b78090e9cd4499c1cac0b8b4a17bb8b
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2021-06-16 22:51:07 +0300

    binary search - split implementation and testing

---
+++ Search_Algorithms/binary_search_tests.py
@@ -0,0 +1,12 @@
+from binary_search import *
+
+
+L = [2, 5, 6, 8, 9, 23, 45, 66, 78, 125, 456]
+K = [3, 4, 10]
+print(L)
+for i in range(len(L)): 
+    print("Search for {0:3d} which is in L with index {1:3d} | binary_search(L,{0:3d}) ->  {2:}"\
+        .format(L[i], L.index(L[i]), binary_search(L, L[i])))
+for i in range(len(K)):
+    print("Search for {0:3d} which is not in L | binary_search(L,{0:3d}) ->  {1:}"\
+        .format(K[i], binary_search(L, K[i])))
